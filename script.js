59342f18b3c33138f0233ec9dce8e29fc8d68482
subhaishere <60627114+subhaishere@users.noreply.github.com>
2023-08-09 20:06:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,32 +1,21 @@
-let arr = ['Tajmahal', 'Victoria Memorial', 'The Virupaksha Temple'];
-let updatedList = arr.map((element) => {
-	// ["a", "some", "one"]
-	let words = element.split(" ") ;
-	let updatedString = words.reduce((prev, current, index) => {
-		if(current != "The" && current != "an" && current != "a"){
-			return prev + " " + current ;
-		}
-		return prev ;
-	}, "")
-
-	return updatedString ;
-}) ;
-let mp = {} ; // {"some one" : "a some one"}
-updatedList.forEach( (element, index) => {
-	mp[element] = arr[index] ;
-});
-// ["some one" , "mno abc" , "abc kh"]
-
-updatedList.sort(); 
-
-let finalAns = updatedList.map((element) => {
-    return mp[element] ;
-})
-
-const ul = document.getElementById("band")
-
-finalAns.forEach((item) => {
-	const li = document.createElement("li");
-	li.innerText = item ;
-	ul.append(li);
-})
+//your code here
+let bandNames=['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal']
+let articles = ['A', 'AN', 'THE']
+ 
+	bandNames.sort((a,b) => (strip(a)> strip(b))? 1: -1)
+	const ref= document.getElementsByTagName("ul")[0] 
+	for (let i=0;i<bandNames.length; i++){
+		const li = document.createElement("li")
+		li.innerText= bandNames [i]
+		ref.append(li)
+	}
+function strip(word){
+	let arr = word.split(" ")
+	let s=""
+	for(let i=0;i<arr.length;i++){
+if(articles.indexOf(arr[i].toUpperCase()) === -1){
+	s=s+arr[i]
+}
+}
+return s.trim()
+}
